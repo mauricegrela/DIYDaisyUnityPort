745ec02a1fc4b02bc842bfd45bdc4654cf2c4745
Maurice Grela <maurice.grela@gmail.com>
2018-06-21 11:03:35 -0700

    Committing the the git ignore and implemented feed back

---
+++ Assets/Scripts/MyIAPManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class MyIAPManager :MonoBehaviour, IStoreListener
+{
+    private IStoreController controller;
+    private IExtensionProvider extensions;
+
+    void Start(){
+
+        /*StandardPurchasingModule module = StandardPurchasingModule.Instance();
+        ConfigurationBuilder builder = ConfigurationBuilder.Instance(module);
+        builder.AddProduct("com.imaginecreatemedia.daisy.StickerSet", ProductType.NonConsumable);
+        UnityPurchasing.Initialize(this, builder);
+        Debug.Log("Working");*/
+
+        //TestBuild();
+    }
+
+    /*public MyIAPManager()
+    {
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        builder.AddProduct("100_gold_coins", ProductType.Consumable, new IDs
+        {
+            {"100_gold_coins_google", GooglePlay.Name},
+            {"100_gold_coins_mac", MacAppStore.Name}
+        });
+
+        UnityPurchasing.Initialize(this, builder);
+    }*/
+
+    /// <summary>
+    /// Called when Unity IAP is ready to make purchases.
+    /// </summary>
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        this.controller = controller;
+        this.extensions = extensions;
+    }
+
+    /// <summary>
+    /// Called when Unity IAP encounters an unrecoverable initialization error.
+    ///
+    /// Note that this will not be called if Internet is unavailable; Unity IAP
+    /// will attempt initialization until it becomes available.
+    /// </summary>
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+    }
+
+    /// <summary>
+    /// Called when a purchase completes.
+    ///
+    /// May be called at any time after OnInitialized().
+    /// </summary>
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    {
+        return PurchaseProcessingResult.Complete;
+    }
+
+    /// <summary>
+    /// Called when a purchase fails.
+    /// </summary>
+    public void OnPurchaseFailed(Product i, PurchaseFailureReason p)
+    {
+    }
+
+    public void TestBuild()
+    {
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        builder.AddProduct("com.imaginecreatemedia.daisy.StickerSet", ProductType.NonConsumable);
+
+        UnityPurchasing.Initialize(this, builder);
+        Debug.Log("Working");
+    }
+
+}
