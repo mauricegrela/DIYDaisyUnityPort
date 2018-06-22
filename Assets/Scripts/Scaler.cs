6aba7a139200504804321f6f55d2d5aa2ebcf5c2
Maurice Grela <maurice.grela@gmail.com>
2018-06-22 12:24:19 -0700

    Committing implemented feedback.

---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour {
+
+    private CanvasScaler ScaleRef;
+	// Use this for initialization
+	void Start () {
+        
+        ScaleRef = GetComponent<CanvasScaler>();
+
+        if ((UnityEngine.iOS.Device.generation.ToString()).IndexOf("iPad") > -1)
+        {
+            ScaleRef.matchWidthOrHeight = 0;
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
